7657ea3df16795b32aa86f4483a1ea01905a4dc6
Leonardo <leomontagnolli@gmail.com>
2020-05-16 20:23:49 -0300

    Adicionado metodo put e delete, tratamento de erros com optional.

--- forum/src/main/java/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/com/alura/forum/controller/TopicosController.java
@@ -3,14 +3,18 @@ package com.alura.forum.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +22,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import com.alura.forum.controller.dto.DetalhesDoTopicoDto;
 import com.alura.forum.controller.dto.TopicoDto;
+import com.alura.forum.controller.form.AtualizarTopicoForm;
 import com.alura.forum.controller.form.TopicoForm;
 import com.alura.forum.model.Topico;
 import com.alura.forum.repository.CursoRepository;
@@ -46,20 +51,50 @@ public class TopicosController {
 	}
 	
 	@GetMapping("/{id}")
-	public DetalhesDoTopicoDto detalhar(@PathVariable Long id) {
-			Topico topico = topicoRepository.getOne(id);
+	public ResponseEntity<DetalhesDoTopicoDto> detalhar(@PathVariable Long id) {
+			Optional<Topico> topico = topicoRepository.findById(id);
+			if(topico.isPresent()) {
+				return ResponseEntity.ok(new DetalhesDoTopicoDto(topico.get()));
+			}
 			
-			return new DetalhesDoTopicoDto(topico);
+		return ResponseEntity.notFound().build();
+			
 		
 	}
 	
 	 @PostMapping
+	 @Transactional
 	 public ResponseEntity<TopicoDto> cadastrar (@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder)	{
 		 Topico topico = form.converter(cursoRepository);
 		 topicoRepository.save(topico);
 		 URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
 		 return ResponseEntity.created(uri).body(new TopicoDto(topico));
 		 
+	 }
+	 
+	 @PutMapping("{id}")
+	 @Transactional
+	 public ResponseEntity<TopicoDto> Atualizar (@PathVariable Long id, @RequestBody @Valid AtualizarTopicoForm form) {
+		 Optional<Topico> optional = topicoRepository.findById(id);
+			if(optional.isPresent()) {
+				Topico topico = form.atualizar(id, topicoRepository);
+				return ResponseEntity.ok(new TopicoDto(topico));
+			}
+		 
+		return ResponseEntity.notFound().build();	 
+	 }
+	 
+	 @DeleteMapping("/{id}")
+	 @Transactional
+	 public ResponseEntity<TopicoDto> remover (@PathVariable Long id ) {
+		 Optional<Topico> optional = topicoRepository.findById(id);
+		 if(optional.isPresent()) {
+			 topicoRepository.deleteById(id);
+			 return ResponseEntity.ok().build();
+		 }
+		
+		 return ResponseEntity.notFound().build();	 
+		
 	 }
 	
 		
---
+++ forum/src/main/java/com/alura/forum/controller/form/AtualizarTopicoForm.java
@@ -0,0 +1,62 @@
+package com.alura.forum.controller.form;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.alura.forum.model.Topico;
+import com.alura.forum.repository.TopicoRepository;
+
+public class AtualizarTopicoForm {
+	
+	@NotNull @NotEmpty
+	private String titulo;
+	@NotNull @NotEmpty @Length()
+	private String mensagem;
+	
+	
+	
+	
+	
+	public String getTitulo() {
+		return titulo;
+	}
+
+
+
+
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+
+
+
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+
+
+
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+
+
+
+
+
+	public Topico atualizar(Long id, TopicoRepository topicoRepository) {
+		Topico topico = topicoRepository.getOne(id);
+		topico.setTitulo(this.titulo);
+		topico.setMensagem(this.mensagem);
+		
+		return topico;
+	}
+
+}
