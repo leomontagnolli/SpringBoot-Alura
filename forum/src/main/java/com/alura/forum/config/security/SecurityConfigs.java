3ff9be18371ffdbda6c06091d7df959a9ca7b408
Leonardo <leomontagnolli@gmail.com>
2020-05-20 20:40:08 -0300

    Adicionando spring security ao projeto

---
+++ forum/src/main/java/com/alura/forum/config/security/SecurityConfigs.java
@@ -0,0 +1,15 @@
+package com.alura.forum.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigs extends WebSecurityConfigurerAdapter {
+	
+	
+	
+	
+
+}
