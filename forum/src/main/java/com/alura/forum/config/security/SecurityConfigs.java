4b713ed4331bf9fbf898f6e0d31d33167a3fdd7f
Leonardo <leomontagnolli@gmail.com>
2020-05-20 21:01:53 -0300

    Authorizations

--- forum/src/main/java/com/alura/forum/config/security/SecurityConfigs.java
+++ forum/src/main/java/com/alura/forum/config/security/SecurityConfigs.java
@@ -1,6 +1,10 @@
 package com.alura.forum.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -9,6 +13,29 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfigs extends WebSecurityConfigurerAdapter {
 	
 	
+	//configuracoes de autenticacao
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+	}
+	
+	//configuracoes de autorizacao
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET ,"/topicos").permitAll()
+		.antMatchers(HttpMethod.GET, "/topicos/").permitAll()
+		.anyRequest().authenticated()
+		.and().formLogin();
+		
+
+	}
+	
+	//configuracoes de recursos estaticos
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+
+	}
 	
 	
 
